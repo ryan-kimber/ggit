590962c3c6556779dca42cb2779aac70c7ea06b0
Ryan Kimber <ryan.kimber@gmail.com>
2016-03-19 22:49:31 -0400

    Update commit-message.js

--- src/commit-message.js
+++ src/commit-message.js
@@ -8,7 +8,6 @@ var Q = require('q');
     var commitMsgFile = filename;  
     if(!fileInfo('./.git').isDirectory()) { 
       var unparsedText = "" + read('./.git');
-      console.log("ParsedText: " + unparsedText.substring('gitdir: '.length).trim());
       commitMsgFile = unparsedText.substring('gitdir: '.length).trim() + '/COMMIT_EDITMSG';
     }   
 
